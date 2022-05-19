c82afbf096f591de8f831393ede66b74ad66cc02
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-05-18 17:40:37 -0700

    got pokemon data fetching via search function

---
+++ src/PokemonList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PokemonList({ monsters }) {
+  return (
+    <div>
+      {monsters.map()}
+    </div>
+  );
+}
---
+++ src/services/fetch-utils.js
@@ -0,0 +1,8 @@
+export async function getMonsters(searchFilter) {
+
+  const rawResponse = await fetch(`/.netlify/functions/pokemon?pokemon=${searchFilter}`);
+  const data = await rawResponse.json();
+  
+  return data.data.results;
+}
+  
